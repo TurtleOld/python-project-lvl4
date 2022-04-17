0eeeabc98b4203cdcd304b3c951f3dfcf65cd1a8
TurtleOld <shurpo.alexander@gmail.com>
2022-04-17 22:36:52 +0300

    added static

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -139,8 +139,9 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'),)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
