10a5abb6791e3ccc1b851bf4bd99abda3b40e5ef
Alexander <dev@pavlovteam.ru>
2022-04-27 23:20:37 +0300

    change model Users = AbstractUser

--- users/models.py
+++ users/models.py
@@ -1,10 +1,6 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-class Users(models.Model):
-    username = models.CharField(max_length=15)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
-    email = models.CharField(max_length=40)
+class User(AbstractUser):
+    pass
