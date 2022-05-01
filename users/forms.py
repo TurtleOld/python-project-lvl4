6ad2dfabc2efd1e44b91ff700f75c780a7e25d63
Alexander <dev@pavlovteam.ru>
2022-05-01 15:40:48 +0300

    delete form UpdateUserForm

--- users/forms.py
+++ users/forms.py
@@ -12,9 +12,3 @@ class AuthUserForm(AuthenticationForm):
     username = User.username
     password = User.password
     fields = ['username', 'password1']
-
-
-class UpdateUserForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
