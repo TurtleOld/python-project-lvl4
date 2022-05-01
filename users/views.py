0a9e61c963e29755d2c0d4040885bd2901805095
Alexander <dev@pavlovteam.ru>
2022-05-01 19:30:51 +0300

    change form to bootstrap

--- users/views.py
+++ users/views.py
@@ -1,9 +1,14 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, \
+    UserPassesTestMixin, AccessMixin
 from django.contrib.auth.views import LoginView
+from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, \
+    FormView
 from users.models import User
 from users.forms import RegisterUserForm, AuthUserForm
 
@@ -31,16 +36,45 @@ class LoginUser(LoginView, SuccessMessageMixin):
 
 class UpdateUser(LoginRequiredMixin,
                  SuccessMessageMixin,
-                 UpdateView,):
+                 UserPassesTestMixin,
+                 AccessMixin,
+                 UpdateView,
+                 FormView,):
     model = User
     template_name = 'users/update.html'
     form_class = RegisterUserForm
     success_url = reverse_lazy('users:list')
+    error_message = 'You do not have permission to change another user'
+    no_permission_url = 'users:list'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['button_text'] = gettext('Изменить пользователя')
+        return context
+
+    def test_func(self):
+        return self.request.user == self.get_object()
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.error_message)
+        return redirect(self.no_permission_url)
 
 
 class DeleteUser(LoginRequiredMixin,
                  SuccessMessageMixin,
-                 DeleteView,):
+                 UserPassesTestMixin,
+                 AccessMixin,
+                 DeleteView,
+                 FormView,):
     model = User
     template_name = 'users/delete.html'
     success_url = reverse_lazy('users:list')
+    error_message = 'You do not have permission to change another user'
+    no_permission_url = 'users:list'
+
+    def test_func(self):
+        return self.request.user == self.get_object()
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.error_message)
+        return redirect(self.no_permission_url)
