96629261b08dadb0fd2872a0577aedc747c1e2d5
Alexander <dev@pavlovteam.ru>
2022-05-01 15:43:16 +0300

    added views Delete User and refactoring: added function reverse_lazy

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from users.models import User
-from users.forms import RegisterUserForm, AuthUserForm, UpdateUserForm
+from users.forms import RegisterUserForm, AuthUserForm
 
 
 # Create your views here.
@@ -17,18 +19,28 @@ class CreateUser(CreateView, SuccessMessageMixin):
     model = User
     template_name = 'users/register.html'
     form_class = RegisterUserForm
-    success_url = '/login/'
+    success_url = reverse_lazy('login')
 
 
 class LoginUser(LoginView, SuccessMessageMixin):
     model = User
     template_name = 'users/login.html'
-    success_url = '/'
     form_class = AuthUserForm
+    success_url = reverse_lazy('')
 
 
-class UpdateUser(UpdateView, SuccessMessageMixin):
+class UpdateUser(LoginRequiredMixin,
+                 SuccessMessageMixin,
+                 UpdateView,):
     model = User
     template_name = 'users/update.html'
-    form_class = UpdateUserForm
-    success_url = '/'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('users:list')
+
+
+class DeleteUser(LoginRequiredMixin,
+                 SuccessMessageMixin,
+                 DeleteView,):
+    model = User
+    template_name = 'users/delete.html'
+    success_url = reverse_lazy('users:list')
