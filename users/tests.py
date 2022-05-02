af01fc64034b40962129b74dd989a6b56e2844a2
Alexander <dev@pavlovteam.ru>
2022-05-02 23:50:35 +0300

    added test

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
+from faker import Faker
 
 from users.models import User
 
@@ -12,17 +13,27 @@ class TestUser(TestCase):
     def setUp(self) -> None:
         self.user1 = User.objects.get(pk=1)
         self.user2 = User.objects.get(pk=2)
+        self.client: Client = Client()
+        self.faker = Faker()
 
     def test_create_user(self):
         url = reverse('users:create')
-        print(url)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        # create new user
+        Faker.seed(0)
+        username = self.faker.user_name()
+        first_name = self.faker.first_name()
+        last_name = self.faker.last_name()
+        set_password = self.faker.password(length=12)
         new_user = {
-            'username': 'alexander',
-            'first_name': 'Alexander',
-            'last_name': 'Pavlov',
-            'password1': '123',
-            'password2': '123',
+            'first_name': first_name,
+            'last_name': last_name,
+            'username': username,
+            'password1': set_password,
+            'password2': set_password,
         }
+
         response = self.client.post(url, new_user, follow=True)
-        print(response.status_code)
         self.assertRedirects(response, '/login/')
