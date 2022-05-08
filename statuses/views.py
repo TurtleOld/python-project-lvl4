d1724bfef0ac71a5fc58856c7929afc38704d369
Alexander <dev@pavlovteam.ru>
2022-05-08 23:35:32 +0300

    refactoring

--- statuses/views.py
+++ statuses/views.py
@@ -1,6 +1,5 @@
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin, \
-    UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
