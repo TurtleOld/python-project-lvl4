715cdada13cc21bac6f35aa01e8e6586c69096ac
Alexander <dev@pavlovteam.ru>
2022-05-08 23:18:32 +0300

    added two classes - UpdateStatus and DeleteStatus

--- statuses/views.py
+++ statuses/views.py
@@ -1,10 +1,12 @@
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin, \
+    UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext, gettext_lazy
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, FormView, \
+    DeleteView
 
 from statuses.forms import StatusForm
 from statuses.models import Status
@@ -32,14 +34,50 @@ class StatusCreate(SuccessMessageMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['title'] = gettext('Создание статуса')
         context['button_text'] = gettext('Создать статус')
         context['label'] = gettext('Имя')
         return context
 
 
-class UpdateStatus(SuccessMessageMixin, UpdateView):
-    pass
+class UpdateStatus(LoginRequiredMixin,
+                   SuccessMessageMixin,
+                   AccessMixin,
+                   UpdateView,
+                   FormView, ):
+    model = Status
+    template_name = 'statuses/update.html'
+    form_class = StatusForm
+    success_url = reverse_lazy('statuses:list')
+    success_message = gettext_lazy('Статус успешно обновлен')
+    error_message = gettext_lazy('У вас нет разрешения на изменение статуса')
+    no_permission_url = 'statuses:list'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = gettext('Изменение статуса')
+        context['button_text'] = gettext('Изменить статус')
+        return context
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.error_message)
+        return redirect(self.no_permission_url)
 
 
-class DeleteStatus(SuccessMessageMixin, UpdateView):
-    pass
+class DeleteStatus(LoginRequiredMixin,
+                   SuccessMessageMixin,
+                   AccessMixin,
+                   DeleteView,
+                   FormView, ):
+    model = Status
+    template_name = 'statuses/delete.html'
+    success_url = reverse_lazy('statuses:list')
+
+    def form_valid(self, form):
+        if self.get_object().tasks.all():
+            messages.error(self.request, gettext_lazy('Вы не можете удалить '
+                                                      'статус, потому что он '
+                                                      'используется'))
+        else:
+            super(DeleteStatus, self).form_valid(form)
+        return redirect(self.success_url)
