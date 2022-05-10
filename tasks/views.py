8b1e3acb43457674d3b3ed60d9f629af9071d2df
Alexander <dev@pavlovteam.ru>
2022-05-10 18:00:54 +0300

    added classes TasksList, TaskCreate, TaskUpdate, TaskDelete

---
+++ tasks/views.py
@@ -0,0 +1,93 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.utils.translation import gettext, gettext_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from tasks.forms import TaskForm
+from tasks.models import Task
+from users.models import User
+
+
+class TasksList(LoginRequiredMixin,
+                SuccessMessageMixin,
+                ListView,
+                AccessMixin):
+    model = Task
+    template_name = 'tasks/list_tasks.html'
+    context_object_name = 'tasks'
+    error_message = gettext('У вас нет прав на просмотр данной страницы! '
+                            'Авторизуйтесь!')
+    no_permission_url = reverse_lazy('login')
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.error_message)
+        return redirect(self.no_permission_url)
+
+
+class TaskCreate(SuccessMessageMixin, CreateView):
+    model = Task
+    template_name = 'tasks/create_task.html'
+    form_class = TaskForm
+    success_message = gettext_lazy('Задача успешно создана!')
+    success_url = reverse_lazy('task:list')
+    error_message = gettext('У вас нет прав на просмотр данной страницы! '
+                            'Авторизуйтесь!')
+    no_permission_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        form.instance.author = User.objects.get(pk=self.request.user.pk)
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(TaskCreate, self).get_context_data(**kwargs)
+        context['title'] = gettext('Создание задачи')
+        context['button_text'] = gettext('Создать задачу')
+        return context
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.error_message)
+        return redirect(self.no_permission_url)
+
+
+class TaskUpdate(SuccessMessageMixin, UpdateView):
+    model = Task
+    template_name = 'tasks/update_task.html'
+    form_class = TaskForm
+    success_message = gettext_lazy('Задача успешно создана!')
+    success_url = reverse_lazy('task:list')
+    error_message = gettext('У вас нет прав на просмотр данной страницы! '
+                            'Авторизуйтесь!')
+    no_permission_url = reverse_lazy('login')
+
+    def get_context_data(self, **kwargs):
+        context = super(TaskUpdate, self).get_context_data(**kwargs)
+        context['title'] = gettext_lazy('Обновление задачи')
+        context['button_text'] = gettext_lazy('Обновить задачу')
+        return context
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.error_message)
+        return redirect(self.no_permission_url)
+
+
+class TaskDelete(SuccessMessageMixin, DeleteView):
+    model = Task
+    template_name = 'tasks/delete_task.html'
+    success_url = reverse_lazy('task:list')
+
+    def get_context_data(self, **kwargs):
+        context = super(TaskDelete, self).get_context_data(**kwargs)
+        context['title'] = gettext_lazy('Удаление задачи')
+        context['button_text'] = gettext_lazy('Удалить задачу')
+        return context
+
+    def form_valid(self, form):
+        if self.request.user != self.get_object().author:
+            messages.error(self.request, gettext_lazy('Вы не можете удалить '
+                                                      'чужую задачу!'))
+        else:
+            super(TaskDelete, self).form_valid(form)
+        return redirect(self.success_url)
