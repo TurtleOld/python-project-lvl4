d6e5fa12616168a68dde3603ce0543981cccde95
Alexander <dev@pavlovteam.ru>
2022-05-10 18:01:17 +0300

    added url to tasks

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 from task_manager import settings
@@ -24,6 +24,8 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('users/', include('users.urls'), name='users_list'),
     path('statuses/', include('statuses.urls'), name='statuses_list'),
+    re_path(r'tasks/',
+            include('tasks.urls', namespace='task'), name='tasks_list'),
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', LogoutUser.as_view(),
          {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
