47cba40befbce2855bde304d9df22e47fbc0caad
Alexander <dev@pavlovteam.ru>
2022-05-15 21:18:17 +0300

    change urls

--- labels/urls.py
+++ labels/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from statuses.views import StatusList, StatusCreate, UpdateStatus, DeleteStatus
+from labels.views import LabelsList, LabelCreate, LabelUpdate, LabelDelete
 
 app_name = 'labels'
 urlpatterns = [
     path('', LabelsList.as_view(), name='list'),
     path('create/', LabelCreate.as_view(), name='create'),
-    path('<int:pk>/update/', UpdateLabel.as_view(), name='update_label'),
-    path('<int:pk>/delete/', DeleteLabel.as_view(), name='delete_label'),
+    path('<int:pk>/update/', LabelUpdate.as_view(), name='update_label'),
+    path('<int:pk>/delete/', LabelDelete.as_view(), name='delete_label'),
 ]
