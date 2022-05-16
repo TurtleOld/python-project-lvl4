20fa179cc5dd21b89872f2d25cd3cc87f743e8e6
Alexander <dev@pavlovteam.ru>
2022-05-16 06:03:13 +0300

    refactor

--- labels/models.py
+++ labels/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Label(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
