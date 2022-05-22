ad0c2437941757e189100985875a7ffd5077ef1f
Alexander <dev@pavlovteam.ru>
2022-05-22 23:27:56 +0300

    changed text message

--- users/views.py
+++ users/views.py
@@ -36,7 +36,7 @@ class LoginUser(SuccessMessageMixin, LoginView):
     model = User
     template_name = 'users/login.html'
     form_class = AuthUserForm
-    success_message = gettext_lazy('Вход успешно выполнен')
+    success_message = gettext_lazy('Вы залогинены')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
