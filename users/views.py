d76aeec7ba33b2219e3adc55e817ffa9fc106f76
Alexander <dev@pavlovteam.ru>
2022-05-23 06:20:46 +0300

    refactoring

--- users/views.py
+++ users/views.py
@@ -70,7 +70,7 @@ class UpdateUser(LoginRequiredMixin,
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['button_text'] = gettext('Изменить пользователя')
+        context['button_text'] = gettext('Изменить')
         return context
 
     def test_func(self):
