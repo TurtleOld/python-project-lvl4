489c357fddac0418212f3b0d26b4639a65f03188
Alexander <dev@pavlovteam.ru>
2022-05-29 19:05:49 +0300

    added delete method

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,11 +1,15 @@
+from django import http
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.db.models.deletion import ProtectedError
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext, gettext_lazy
 from django.views.generic import ListView, CreateView, UpdateView, FormView, \
     DeleteView
+from django.views.generic.edit import DeletionMixin
 
 from task_manager.mixins import HandleNoPermissionMixin
 from task_manager.statuses.forms import StatusForm
@@ -63,17 +67,19 @@ class DeleteStatus(LoginRequiredMixin,
                    SuccessMessageMixin,
                    HandleNoPermissionMixin,
                    DeleteView,
-                   FormView):
+                   DeletionMixin):
     model = Status
     template_name = 'statuses/delete_status.html'
     success_url = reverse_lazy('statuses:list')
     success_message = gettext_lazy('Статус успешно удалён')
+    error_message = gettext_lazy('У вас нет разрешения на изменение статуса')
+    no_permission_url = 'statuses:list'
 
     def form_valid(self, form):
-        if self.get_object().tasks.all():
+        if self.object.tasks.all():
             messages.error(self.request, gettext_lazy('Вы не можете удалить '
                                                       'статус, потому что он '
                                                       'используется'))
         else:
-            super(DeleteStatus, self).form_valid(form)
+            self.object.delete()
         return redirect(self.success_url)
